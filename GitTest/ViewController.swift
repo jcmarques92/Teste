34eeddef1e3c4d370f0efba3af31eefc77b9cbcb
IOS1 <ios1@ios1s-imac.local>
2017-09-25 17:06:30 +0100

    comit 2nd print

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("something")
+        print("something2")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
